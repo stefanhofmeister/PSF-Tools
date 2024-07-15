d161d7fc958a7e4e11ee0582baedf2ed681624e1
stefanhofmeister <47357550+stefanhofmeister@users.noreply.github.com>
2024-07-15 14:32:37 +0900

    Update deconvolve_image.py

--- deconvolve_image.py
+++ deconvolve_image.py
@@ -1,9 +1,4 @@
-"""© 2022 The Trustees of Columbia University in the City of New York.
-This work may be reproduced, distributed, and otherwise exploited for
-academic non-commercial purposes only, provided that it cites the
-original work (IN PUBLICATIONP PROCESS).  To obtain a license to
-use this work for commercial purposes, please contact Columbia
-Technology Ventures at techventures@columbia.edu <mailto:techventures@columbia.edu>. 
+"""© Stefan Hofmeister. 
 """
 
 import numpy as np
